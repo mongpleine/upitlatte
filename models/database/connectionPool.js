559f957d86bddbb0352684f34adf5cf8834d5281
asura3237@gmail.com <asura3237@gmail.com>
2024-09-17 02:18:11 +0900

    <master>2024-09-10: 서버용 설정

--- models/database/connectionPool.js
+++ models/database/connectionPool.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const config = require('../../config/credencial');
 
-let pool = mysql.createPool(config.local_database);
+let pool = mysql.createPool(config.database);
 
 function getConnection(callback) {
     pool.getConnection(function (err, conn) {
