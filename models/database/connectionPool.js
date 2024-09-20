1eeb3434281ec1de9ba2c8f0add37308d9f5f63c
asura3237@gmail.com <asura3237@gmail.com>
2024-09-20 18:31:52 +0900

    <feature/add-cron-jobs>2024-09-20: 설정변경 버그픽스

--- models/database/connectionPool.js
+++ models/database/connectionPool.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
+const credential = require('../../config/credencial');
 
-let pool = mysql.createPool(config.db_selected);
+let pool = mysql.createPool(credential[credential.config.db_selected]);
 
 function getConnection(callback) {
     pool.getConnection(function (err, conn) {
