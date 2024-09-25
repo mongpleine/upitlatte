f562d8da171c44b6e390248722f5d7fa960ff684
mh-lee <asura3237@gmail.com>
2024-09-25 15:25:01 +0900

    <develop>2024-09-25: daterangepicker 적용

--- public/template/mypage/js/chart/productDetail.js
+++ public/template/mypage/js/chart/productDetail.js
@@ -157,3 +157,20 @@ let myLineChart = new Chart(ctx, {
     }
 });
 
+$('#dateRangePicker').daterangepicker({
+    "locale": {
+        "format": "YYYY-MM-DD",
+        "separator": " ~ ",
+        "applyLabel": "확인",
+        "cancelLabel": "취소",
+        "fromLabel": "From",
+        "toLabel": "To",
+        "customRangeLabel": "Custom",
+        "weekLabel": "W",
+        "daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
+        "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    },
+    "drops": "auto"
+}, function (start, end, label) {
+    location.href = `/productDetail?product_id=${selectedData.product_id}&startDate=${start.format('YYYY-MM-DD')}&endDate=${end.format(('YYYY-MM-DD'))}`
+});
